bfc39f600efe2667f348bd543b55f75ff01ab615
Ismael Lucas <ismaellucas36@gmail.com>
2024-09-27 16:00:26 -0300

    Gerando QR code dinamico

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import QrCodeGenerator from "./components/requisicao";
-
+import PdfCom from "../src/components/Pdf"
 function App() {
   const [count, setCount] = useState(0);
 
@@ -30,6 +30,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
       <QrCodeGenerator />
+      <PdfCom/>
     </>
   );
 }
--- src/components/Requisicao.jsx
+++ src/components/Requisicao.jsx
@@ -1,13 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 
-function requisicao() {
+function Requisicao() {
+  const [codigo, setCodigo] = useState(""); // Estado para armazenar o código
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    const codigoValue = window.document.getElementById("codigo").value;
+    setCodigo(codigoValue); // Atualiza o estado
+    console.log(codigoValue); // Mostra o valor no console
+  }
+
   return (
-    <img
-      src="https://api.qrserver.com/v1/create-qr-code/?data=JAca Mole&amp;size=100x100"
-      alt=""
-      title=""
-    />
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Seu código:
+          <input type="text" id="codigo" name="name" />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+      
+      {codigo && ( // Mostra a imagem se o código for inserido
+        <img
+          src={`https://api.qrserver.com/v1/create-qr-code/?data=https://publicidadelegaldp.com.br/${codigo}&size=100x100`}
+          alt="QR Code"
+          title="QR Code"
+        />
+      )}
+    </div>
   );
 }
 
-export default requisicao;
+export default Requisicao;

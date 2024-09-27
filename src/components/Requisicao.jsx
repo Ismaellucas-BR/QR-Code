2a3330e123eab8d5cae07b4481fd3ca070543084
Ismaellucas-BR <ismaellucas36@gmail.com>
2024-09-27 07:32:41 -0300

    Criando o QR Code

---
+++ src/components/Requisicao.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function requisicao() {
+  return (
+    <img
+      src="https://api.qrserver.com/v1/create-qr-code/?data=JAca Mole&amp;size=100x100"
+      alt=""
+      title=""
+    />
+  );
+}
+
+export default requisicao;
